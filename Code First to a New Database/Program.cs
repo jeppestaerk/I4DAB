dccf5280899c3647c3db5a96f27e30179171f4a5
jeppe <jeppe@DESKTOP-E0NEOMP>
2016-09-02 09:50:10 +0200

    Add project files.

---
+++ Code First to a New Database/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_First_to_a_New_Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
