c4e14d46fa4e343a0dfbb731ad0fae404895a8b9
Jeppe Stærk <jeppe@staerk.io>
2016-11-29 09:03:49 +0100

    DBContext added

---
+++ HandIn4/Models/HandIn4DbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace HandIn4.Models
+{
+  class HandIn4DbContext : DbContext
+  {
+    public DbSet<Reading> Readings { get; set; }
+    public DbSet<Appartmentcharacteristic> Appartmentcharacteristics { get; set; }
+    public DbSet<Sensorcharacteristic> Sensorcharacteristics { get; set; }
+
+  }
+}
